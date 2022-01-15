a7e04b145631dd275a3aafb414991f27a55daa24
Ahmet Berkay Seyhun <60739501+BerkaySeyhun@users.noreply.github.com>
2022-01-15 20:42:52 +0300

    Update Pisagor.java

--- Java/Pisagor.java
+++ Java/Pisagor.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg7.pisagor;
 
 import java.util.Scanner;
 
-/**
- *
- * @author ebube
- */
 public class Pisagor {
 
     public static void main(String[] args) {
